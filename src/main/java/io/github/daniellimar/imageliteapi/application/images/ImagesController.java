8f5737e440d32884522eb04ec038e35b6addd9dd
Daniel Rodrigues de Lima <daniel.engca@outlook.com>
2024-06-19 07:03:41 -0400

    Feature new Entity\Image

--- src/main/java/io/github/daniellimar/imageliteapi/application/images/ImagesController.java
+++ src/main/java/io/github/daniellimar/imageliteapi/application/images/ImagesController.java
@@ -1,4 +1,4 @@
-package io.github.daniellimar.imageliteapi.application;
+package io.github.daniellimar.imageliteapi.application.images;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
---
+++ src/main/java/io/github/daniellimar/imageliteapi/domain/enums/ImageExtension.java
@@ -0,0 +1,8 @@
+package io.github.daniellimar.imageliteapi.domain.enums;
+
+public enum ImageExtension {
+    PNG,
+    JPG,
+    JPEG,
+    GIF
+}
